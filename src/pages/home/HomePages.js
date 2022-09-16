ba5e6a6cf19572794a5d963616e2f6f6d073360b
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2022-09-16 12:38:51 +0700

    Testing logic

---
+++ src/pages/home/HomePages.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePages = () => {
+    
+  return (
+    <div>
+      Ini HomePages
+    </div>
+  )
+}
---
+++ src/pages/user-detail/UserDetailPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const UserDetailPage = () => {
+    
+  return (
+    <div>
+      Ini user detail page
+    </div>
+  )
+}
